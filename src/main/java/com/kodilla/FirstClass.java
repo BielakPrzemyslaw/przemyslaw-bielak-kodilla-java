c4d1e967d20b6593f07c9d5b736744a4aa0393e0
BielakPrzemyslaw <https://github.com/BielakPrzemyslaw/przemyslaw-bielak-kodilla-java.git>
2019-12-19 18:59:18 +0100

    update

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,6 +1,21 @@
 package com.kodilla;
-//First class
+class SimpleSubstract
+{
+    public double substractBFromA(double a, double b) {
 
-public class FirstClass {
-    int = 1;
+        return a * b;
+    }
 }
+
+class Aplication
+{
+    public static void main (String[] args) {
+
+        SimpleSubstract calculator = new SimpleSubstract();
+
+        double result = calculator.substractBFromA(16, 2);
+
+        System.out.println(result);
+    }
+}
+
