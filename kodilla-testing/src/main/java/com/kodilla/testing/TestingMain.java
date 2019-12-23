aa03d7a2ceb59e41e02e9e4aa873841b63861c94
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2019-12-23 22:32:08 +0100

    Kalkulator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Testing calculator
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(6,8);
+        int subResult = calculator.subtract(40,10);
+
+                if((addResult == 14)&&(subResult == 30)) {
+                    System.out.println("Calculator works !!!");
+                }else{
+                    System.out.println("Error !!!");
+            }
+
+        }
     }
-}
