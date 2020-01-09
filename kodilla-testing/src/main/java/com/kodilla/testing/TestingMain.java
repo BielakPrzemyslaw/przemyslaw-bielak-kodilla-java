8df2a99903bd492b27cf54c39ecb1076d4fd8514
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2020-01-09 20:48:47 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,23 +1,20 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
+       /* SimpleUser simpleUser = new SimpleUser();
 
         String result = simpleUser.getUsername();
 
         if (result.equals("theForumUser")){
             System.out.println("test OK");
         } else {
-            System.out.println("Error!");
+           System.out.println("Error!");
         }
 
         //Testing calculator
         Calculator calculator = new Calculator();
-        int addResult = calculator.add(6,8);
+       int addResult = calculator.add(6,8);
         int subResult = calculator.subtract(40,10);
 
                 if((addResult == 14)&&(subResult == 30)) {
@@ -25,6 +22,6 @@ public class TestingMain {
                 }else{
                     System.out.println("Error !!!");
             }
-
+*/
         }
     }
