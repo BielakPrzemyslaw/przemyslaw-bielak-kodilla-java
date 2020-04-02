d6a286da08e78afaf8503ef61a72662d0176f6af
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2020-04-02 18:23:32 +0200

    Hibernate new gradle

--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -1,18 +1,6 @@
 package com.kodilla.spring;
 
-import com.kodilla.spring.shape.Circle;
-import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Square;
-import com.kodilla.spring.shape.Triangle;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
-
-
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringRunnerTestSuite {
@@ -61,4 +49,4 @@ public class SpringRunnerTestSuite {
         System.out.println("Chosen shape says: " + name);
     }
 
-}
+} */
